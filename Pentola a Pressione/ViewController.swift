a67c87c146ad9e56ba168a55644106c9e13bc0f6
Alessio Cuccovillo <alessio.cuccovillo@gmail.com>
2017-05-24 23:38:50 +0200

    Initial Commit

---
+++ Pentola a Pressione/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pentola a Pressione
+//
+//  Created by Alessio Cuccovillo on 24/05/17.
+//  Copyright © 2017 Alessio Cuccovillo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
